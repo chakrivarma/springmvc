3a51814f2c8f61ca15acaff2494fb912b23f4300
chaitanyavarma.m <chaitanyavarma.m@ABCOM325.anthembio.com>
2018-04-09 20:10:08 +0530

    spring web mvc

---
+++ src/main/java/controllers/ControllerTest.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ControllerTest {
+	@RequestMapping("/welcome")
+	public ModelAndView goToWelcome() {
+		return new ModelAndView("/welcome");
+	}
+}
